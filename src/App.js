665c0270191b750a36651ff212a60e1c782b8b6a
PeppZZon <jespersimonlind@gmail.com>
2022-12-23 11:50:07 +0100

    Added Profile component to router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login";
+import { Profile } from "./components/Profile";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
         </Routes>
       </BrowserRouter>
     </>
