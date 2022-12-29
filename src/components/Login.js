83986e2f61096837b39135b4b00999fa0ddb0f07
PeppZZon <jespersimonlind@gmail.com>
2022-12-29 17:11:58 +0100

    Created Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,37 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { signInWithGoogle } from "../firebase-config";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "../firebase-config";
+import { client } from "../client";
 
 export const Login = () => {
   const navigate = useNavigate();
+
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const createUserProfile = async (user) => {
+    try {
+      const userInfo = {
+        _id: user.uid,
+        _type: "user",
+        userName: user.displayName,
+        image: user.photoURL,
+      };
+      client.createIfNotExists(userInfo).then(() => {
+        navigate("/profile");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      createUserProfile(user.user);
+    }
+  }, [user]);
   return (
     <>
-      <button onClick={signInWithGoogle}>Sign in with google</button>
+      <button onClick={() => signInWithGoogle()}>Sign in with google</button>
     </>
   );
 };
