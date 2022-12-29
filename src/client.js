0b4ba8a99eaec65038a4c049adf69600cd27633a
PeppZZon <jespersimonlind@gmail.com>
2022-12-29 17:11:21 +0100

    Configured Sanity client

---
+++ src/client.js
@@ -0,0 +1,9 @@
+import sanityClient from "@sanity/client";
+
+export const client = sanityClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  useCdn: true,
+  token:
+    "skJbrroxrqFQ9hKY45y7YbVum1nNwP3mUFTvWqnhPxD2VNNxl7QJjb4RDjHjmAJteBdhNzHzyI9Tkl9JP41IMgSMcXIX1nqZVo03lb9SHxkaqe9dtFOu9jnmnbCeAkR0eEqgbLyO89dAUd1fQLUPpxKij3ZzsJUmE3ydILDzdUsbA0SYaW0S",
+});
