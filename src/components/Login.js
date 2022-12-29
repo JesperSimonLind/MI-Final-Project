5956aa09af38581fd912286f8e5c74912d10668b
PeppZZon <jespersimonlind@gmail.com>
2022-12-29 14:07:44 +0100

    Added navigate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { signInWithGoogle } from "../firebase-config";
 
 export const Login = () => {
+  const navigate = useNavigate();
   return (
     <>
       <button onClick={signInWithGoogle}>Sign in with google</button>
