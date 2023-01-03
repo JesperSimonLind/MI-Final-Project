e0910c30cd23180064fa0fc2738b7fb3e52f4b6b
PeppZZon <jespersimonlind@gmail.com>
2023-01-03 16:22:37 +0100

    [post] Created schema for posting images

---
+++ photosharing-studio/schemas/post.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      Title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'userId',
+      title: 'UserId',
+      type: 'string',
+    },
+  ],
+}
