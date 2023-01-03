3f87356cf9e4b2f1e3be024a69a02e99071b7ba3
PeppZZon <jespersimonlind@gmail.com>
2023-01-03 16:23:04 +0100

    [Profile] added some links

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { client } from "../client";
 
 export const Profile = () => {
-  return <>Det här är min profil sida</>;
+  return (
+    <>
+      <h1>Det här är min profil</h1>
+
+      <Link to="/upload">Klick här för att ladda upp en bild</Link>
+    </>
+  );
 };
