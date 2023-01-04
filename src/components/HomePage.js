be6f7c839acd0eb584b05d47f7a63bc0c25f0327
PeppZZon <jespersimonlind@gmail.com>
2023-01-04 14:50:10 +0100

    [Homepage] added jsx stuff

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,35 @@
-import React, { useEffect, useState } from "React";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { client } from "../client";
 import { auth } from "../firebase-config";
 import { userQuery } from "../data/data";
+import { Profile } from "./Profile";
 
 export const HomePage = () => {
   const [user] = useAuthState(auth);
   const [userdb, setUserdb] = useState(null);
 
   useEffect(() => {
-    const query = userQuery(user.uid);
+    const query = userQuery(user?.uid);
     client.fetch(query).then((data) => {
       setUserdb(data[0]);
+      //   console.log(userdb._id + " Hej här är ditt id");
     });
   }, [user]);
 
-  return <></>;
+  return (
+    <>
+      <Link to={`/profile/${userdb?._id}`}>
+        Klicka här för att komma till din profil
+      </Link>
+      {/* <div>
+        <Routes>
+          <Route path="/profile/:userId" element={<Profile />}>
+            Hej
+          </Route>
+        </Routes>
+      </div> */}
+    </>
+  );
 };
