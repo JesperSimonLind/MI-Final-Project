a34cb8654f8023f04abcaa502b4fb45aa62eb6f4
PeppZZon <jespersimonlind@gmail.com>
2023-01-04 14:49:45 +0100

    [Profile] added stuff

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { client } from "../client";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { userQuery } from "../data/data";
+import { auth } from "../firebase-config";
 
-export const Profile = () => {
+export const Profile = ({}) => {
+  const [userCred] = useAuthState(auth);
+  const [user, setUser] = useState();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    const query = userQuery(userId);
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, [userId]);
   return (
     <>
       <h1>Det här är min profil</h1>
+      {/* <h2>{user.userName}</h2>
+      <img src={user.image} /> */}
 
       <Link to="/upload">Klick här för att ladda upp en bild</Link>
     </>
