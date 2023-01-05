3a0e73a9a844fbe6172f983aea88ecdb319830af
PeppZZon <jespersimonlind@gmail.com>
2023-01-05 16:29:25 +0100

    [data] added query for uploaded images

--- src/data/data.js
+++ src/data/data.js
@@ -2,3 +2,15 @@ export const userQuery = (userId) => {
   const query = `*[_type == "user" && _id == '${userId}']`;
   return query;
 };
+
+export const userUploadedImagesQuery = (userId) => {
+  const query = `*[ _type == 'image' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+  }`;
+  return query;
+};
