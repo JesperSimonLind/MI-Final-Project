b32d0e0a8420f651c1985cd393c831e5873f365f
PeppZZon <jespersimonlind@gmail.com>
2023-01-06 16:07:12 +0100

    [Posted] new file, added jsx for showing images

---
+++ src/components/Posted.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { urlFor } from "../client";
+import { auth } from "../firebase-config";
+
+export const Posted = ({ userPost: { createdBy, image } }) => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <img src={urlFor(image).width(200).url()}></img>
+      <img src={createdBy.image}></img>
+      <p>{createdBy.userName}</p>
+      <p>{createdBy._id}</p>
+    </>
+  );
+};
