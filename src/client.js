8f04a4f60b4a46e6f74b37be40579658035cb9f6
PeppZZon <jespersimonlind@gmail.com>
2023-01-06 16:06:01 +0100

    [client] added imagebuilder

--- src/client.js
+++ src/client.js
@@ -1,4 +1,5 @@
 import sanityClient from "@sanity/client";
+import ImageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
@@ -7,3 +8,7 @@ export const client = sanityClient({
   token:
     "skJbrroxrqFQ9hKY45y7YbVum1nNwP3mUFTvWqnhPxD2VNNxl7QJjb4RDjHjmAJteBdhNzHzyI9Tkl9JP41IMgSMcXIX1nqZVo03lb9SHxkaqe9dtFOu9jnmnbCeAkR0eEqgbLyO89dAUd1fQLUPpxKij3ZzsJUmE3ydILDzdUsbA0SYaW0S",
 });
+
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
