02b45111406e4c0da5d20e00af5883ac581add3e
PeppZZon <jespersimonlind@gmail.com>
2023-01-07 11:37:10 +0100

    [HomePage] fetched homequery and added jsx

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { client } from "../client";
 import { auth } from "../firebase-config";
-import { userQuery } from "../data/data";
+import { homeQuery, userQuery } from "../data/data";
+import { PostLayout } from "./PostLayout";
 
 export const HomePage = () => {
   const [user] = useAuthState(auth);
   const [userdb, setUserdb] = useState(null);
+  const [postFeed, setPostFeed] = useState();
 
   useEffect(() => {
     const query = userQuery(user?.uid);
@@ -15,6 +17,13 @@ export const HomePage = () => {
       setUserdb(data[0]);
     });
   }, [user]);
+
+  useEffect(() => {
+    client.fetch(homeQuery).then((data) => {
+      setPostFeed(data);
+    });
+  }, []);
+  if (!postFeed) return <p>Fetching Feed...</p>;
   return (
     <>
       <Link to={`/profile/${userdb?._id}`}>
@@ -25,6 +34,9 @@ export const HomePage = () => {
         {" "}
         Klicka här för att ladda upp en bild
       </Link>
+      <div>
+        <PostLayout createdImages={postFeed}></PostLayout>
+      </div>
     </>
   );
 };
