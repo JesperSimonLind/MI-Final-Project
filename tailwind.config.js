34e99ade937df453e6d7b672a617b254f4920169
PeppZZon <jespersimonlind@gmail.com>
2023-01-10 13:31:26 +0100

    configured background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lgblack: "#0F1424",
+      },
+    },
   },
   plugins: [],
 };
