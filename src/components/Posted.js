b3b349220d80b4f8edc2ce55cb277cbf60935bef
PeppZZon <jespersimonlind@gmail.com>
2023-01-10 13:32:08 +0100

    [Posted] started to style posts

--- src/components/Posted.js
+++ src/components/Posted.js
@@ -3,6 +3,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { urlFor } from "../client";
 import { auth } from "../firebase-config";
+import { FaTrashAlt, FaEdit } from "react-icons/fa";
 
 export const Posted = ({ userPost: { createdBy, image, _id } }) => {
   const [user] = useAuthState(auth);
@@ -10,10 +11,35 @@ export const Posted = ({ userPost: { createdBy, image, _id } }) => {
 
   return (
     <>
-      <img src={urlFor(image).width(200).url()}></img>
-      <img src={createdBy.image}></img>
-      <p>{createdBy.userName}</p>
-      <p>{createdBy._id}</p>
+      {/* <img src={urlFor(image).width(200).url()}></img>
+        <img src={createdBy.image}></img>
+        <p>{createdBy.userName}</p>
+        <p>{createdBy._id}</p> */}
+      <div className="bg-white rounded-2xl">
+        <div className="flex justify-start items-center p-2 gap-3">
+          <img className="rounded-full h-14 w-14" src={createdBy.image}></img>
+          <h2>{createdBy.userName}</h2>
+        </div>
+        <img
+          className="object-scale-down"
+          src={urlFor(image).url()}
+          alt="user-uploaded-image"
+        ></img>
+        <div className="flex justify-center pt-3">
+          <button
+            type="button"
+            className="flex justify-center items-center w-12 h-12"
+          >
+            <FaEdit className="w-10 h-10" />
+          </button>
+          <button
+            className="flex justify-center items-center w-12 h-12"
+            type="button"
+          >
+            <FaTrashAlt className="w-10 h-10" />
+          </button>
+        </div>
+      </div>
     </>
   );
 };
