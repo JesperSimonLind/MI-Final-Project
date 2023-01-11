9ae2244f03c9c072ed07cb4ea01c077b8d0593ad
PeppZZon <jespersimonlind@gmail.com>
2023-01-11 16:51:03 +0100

    [Menu] added logo

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,21 @@ import { Link } from "react-router-dom";
 export const Menu = () => {
   return (
     <>
-      <div className="w-40 h-screen bg-btnBlue flex">
+      <header>
+        <div className="h-20 w-screen bg-lgblack">
+          <div className="flex h-20 items-center justify-center">
+            <Link to="/home">
+              <img
+                className=""
+                src="https://i.pinimg.com/originals/ac/79/98/ac799833b8d540819d2435e037ae5b4f.png"
+                alt="SnapSociety Logo"
+                width="200px"
+              ></img>
+            </Link>
+          </div>
+        </div>
+      </header>
+      {/* <div className="w-40 h-screen bg-btnBlue flex">
         <ul className="relative flex gap-5 flex-col w-full ">
           <li className="relative">
             <Link className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white text-ellipsis whitespace-nowrap rounded hover:bg-btnBlueHover transition duration-300 ease-in-out">
@@ -22,7 +36,7 @@ export const Menu = () => {
             </Link>
           </li>
         </ul>
-      </div>
+      </div> */}
     </>
   );
 };
