376401ecf7fdafb70b162774839ce24ab156576f
PeppZZon <jespersimonlind@gmail.com>
2023-01-11 18:23:59 +0100

    [Menu] added menu icon

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AiOutlineMenu } from "react-icons/ai";
 
 export const Menu = () => {
   return (
     <>
       <header>
         <div className="h-20 w-screen bg-lgblack">
-          <div className="flex h-20 items-center justify-center">
+          <div className="flex h-20 items-center justify-center flex-row">
             <Link to="/home">
               <img
-                className=""
+                className="pl-5"
                 src="https://i.pinimg.com/originals/ac/79/98/ac799833b8d540819d2435e037ae5b4f.png"
                 alt="SnapSociety Logo"
                 width="200px"
               ></img>
             </Link>
+            <div className="flex w-full justify-end pr-8">
+              <button type="button">
+                <AiOutlineMenu className="w-9 h-9 text-white" />
+              </button>
+            </div>
           </div>
         </div>
       </header>
