cfe3cd685658b4b7cdcd06622d7993e594f3d0ef
PeppZZon <jespersimonlind@gmail.com>
2023-01-12 14:25:37 +0100

    [Menu] added toggle

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
-export const Menu = () => {
+export const Menu = ({ user }) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const showMenu = () => {
+    setToggleMenu(true);
+  };
+
+  const closeMenu = () => {
+    setToggleMenu(false);
+  };
   return (
     <>
       <header>
@@ -17,7 +26,7 @@ export const Menu = () => {
               ></img>
             </Link>
             <div className="flex w-full justify-end pr-8">
-              <button type="button">
+              <button onClick={showMenu} type="button">
                 <AiOutlineMenu className="w-9 h-9 text-white" />
               </button>
             </div>
@@ -25,33 +34,43 @@ export const Menu = () => {
         </div>
       </header>
 
-      <nav className="fixed top-0 right-0 bottom-0 left-0 backdrop:-blur-sm z-10">
-        <ul className="absolute top-0 right-0 bottom-0 lg:w-1/6 sm:w-10/12 md:w-1/4  py-4 bg-white drop-shadow-2xl z-10">
-          <li className="border-b border-inherit">
-            <Link to="/" className="block p-4">
-              Jesper Lind
-            </Link>
-          </li>
-          <li className="border-b border-inherit">
-            <Link to="/" className="block p-4">
-              Home
-            </Link>
-          </li>
-          <li className="border-b border-inherit">
-            <Link to="/" className="block p-4">
-              Upload
-            </Link>
-          </li>
-          <li className="border-b border-inherit">
-            <Link to="/" className="block p-4">
-              Logout
-            </Link>
-          </li>
-        </ul>
-        <button className="absolute top-5 right-0 bottom-0 left-2/4 lg:left-3/4 md:left-2/3">
-          <AiOutlineClose className="text-white h-6 w-6 absolute top-2 left-2" />
-        </button>
-      </nav>
+      {toggleMenu && (
+        <nav className="fixed top-0 right-0 bottom-0 left-0 backdrop:-blur-sm z-10">
+          <ul className="absolute top-0 right-0 bottom-0 w-6/12 lg:w-1/6  md:w-1/4  py-4 bg-white drop-shadow-2xl z-10">
+            <li className="border-b border-inherit">
+              {/* <Link to={`user-profile/${user._id}`} className="block p-4">
+              <img
+                src={user.image}
+                className="w-10 h-10 rounded-full"
+                alt="user"
+              ></img>
+              <p>{user.userName}</p>
+            </Link> */}
+            </li>
+            <li className="border-b border-inherit">
+              <Link to="/" className="block p-4">
+                Home
+              </Link>
+            </li>
+            <li className="border-b border-inherit">
+              <Link to="/" className="block p-4">
+                Upload
+              </Link>
+            </li>
+            <li className="border-b border-inherit">
+              <Link to="/" className="block p-4">
+                Logout
+              </Link>
+            </li>
+          </ul>
+          <button
+            onClick={closeMenu}
+            className="absolute top-5 right-0 bottom-0 left-36 lg:left-3/4 md:left-2/3"
+          >
+            <AiOutlineClose className="text-white h-6 w-6 absolute top-2 left-2" />
+          </button>
+        </nav>
+      )}
     </>
   );
 };
