8538c0019caf37761fc927750ad350d4b8765909
PeppZZon <jespersimonlind@gmail.com>
2023-01-13 12:10:58 +0100

    [Posted] Restyled component

--- src/components/Posted.js
+++ src/components/Posted.js
@@ -17,40 +17,38 @@ export const Posted = ({ userPost: { createdBy, image, _id } }) => {
 
   return (
     <>
-      {/* <img src={urlFor(image).width(200).url()}></img>
-        <img src={createdBy.image}></img>
-        <p>{createdBy.userName}</p>
-        <p>{createdBy._id}</p> */}
-      <div className="bg-white rounded-2xl">
+      <div className="rounded xl shadow-lg bg-white w-5/6">
         <div className="flex justify-start items-center p-2 gap-3">
           <img className="rounded-full h-14 w-14" src={createdBy.image}></img>
-          <h2>{createdBy.userName}</h2>
+          <h2 className="text-md">{createdBy.userName}</h2>
         </div>
-        <img
-          className="object-contain"
-          src={urlFor(image).url()}
-          alt="user-uploaded-image"
-        ></img>
-        {createdBy?._id === user?.uid && (
-          <div className="flex justify-center pt-3">
-            <button
-              type="button"
-              className="flex justify-center items-center w-12 h-12"
-            >
-              <FaEdit className="w-10 h-10" />
-            </button>
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                deletePost(_id);
-              }}
-              className=" w-12 h-12"
-              type="button"
-            >
-              <FaTrashAlt className="w-10 h-10" />
-            </button>
+        <div className="p-2 flex-col">
+          <div className="rounded-xl overflow-hidden">
+            <img
+              className="object-contain"
+              src={urlFor(image).url()}
+              alt="user-uploaded-image"
+            ></img>
           </div>
-        )}
+          {createdBy?._id === user?.uid && (
+            <div className="flex justify-center gap-5">
+              <button className="text-center bg-btnBlue text-white py-2 rounded-md mt-4 w-20">
+                <FaEdit className="w-5 h-5 inline-flex" />
+                Edit
+              </button>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deletePost(_id);
+                }}
+                className="text-center bg-btnBlue text-white py-2 rounded-md mt-4 w-20"
+              >
+                <FaTrashAlt className="w-5 h-5 inline-flex" />
+                Delete
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
