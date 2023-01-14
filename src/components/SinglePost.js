1f52e1598e2bfbcf3196f875374df5d69c82edf6
PeppZZon <jespersimonlind@gmail.com>
2023-01-14 13:29:17 +0100

    [SinglePost] added fetch

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { client } from "../client";
 
-export const SinglePost = () => {
+export const SinglePost = ({ user }) => {
+  const { userPostId } = useParams();
+  const [post, setPost] = useState();
+  const [userPostDetail, setUserPostDetail] = useState();
+
+  const fetchUserPostDetails = () => {
+    let query = DetailedPostQuery(userPostId);
+    if (query) {
+      client.fetch(`${query}`).then((data) => {
+        setUserPostDetail(data[0]);
+      });
+    }
+  };
   return <></>;
 };
