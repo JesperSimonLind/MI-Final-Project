6b5e0aee0d400df688df8d14b48a0c2fbca433b2
PeppZZon <jespersimonlind@gmail.com>
2023-01-14 15:38:43 +0100

    [SinglePost] styled component

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -25,12 +25,33 @@ export const SinglePost = ({ user }) => {
   if (!userPostDetail) return <Loader />;
   return (
     <>
-      {/* <h1>{userPostDetail.title}</h1>
-      <h2>{userPostDetail.about}</h2>
-      <img src={userPostDetail.image && urlFor(userPostDetail.image).url()} />
-      <img src={userPostDetail.createdBy.image} />
-      <h2>{userPostDetail.createdBy.userName}</h2> */}
       <Menu />
+      <div className="flex justify-start items-center flex-col h-screen bg-lgblack">
+        <div className="relative w-full h-full">
+          <div className="absolute flex flex-col justify-center top-0 right-0 left-0 bottom-0">
+            <div className="flex justify-center flex-col items-center">
+              <div className="rounded-xl shadow-lg bg-white w-5/6  lg:w-3/6">
+                <div className="flex items-center p-2 gap-3">
+                  <img
+                    className="rounded-full h-14 w-14"
+                    src={userPostDetail.createdBy.image}
+                  ></img>
+                  <h2 className="text-md">
+                    {userPostDetail.createdBy.userName}
+                  </h2>
+                </div>
+                <img
+                  className="pb-4 rounded-lg"
+                  src={userPostDetail && urlFor(userPostDetail.image).url()}
+                  alt="User Posted Image"
+                ></img>
+                <h1 className="text-3xl pl-2">{userPostDetail.title}</h1>
+                <h2 className="pl-2">{userPostDetail.about}</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
