eab75607bac6697512b4ccf86ea97a4d6ac200b7
PeppZZon <jespersimonlind@gmail.com>
2023-01-14 13:06:39 +0100

    [Posted] created toaster

--- src/components/Posted.js
+++ src/components/Posted.js
@@ -4,19 +4,29 @@ import { useNavigate } from "react-router-dom";
 import { client, urlFor } from "../client";
 import { auth } from "../firebase-config";
 import { FaTrashAlt, FaEdit } from "react-icons/fa";
+import toast, { Toaster } from "react-hot-toast";
 
 export const Posted = ({ userPost: { createdBy, image, _id } }) => {
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
   const deletePost = (id) => {
+    toast.success(
+      "Image successfully deleted. Please note that it may take some time before your image disappears",
+      {
+        duration: 5000,
+      }
+    );
     client.delete(id).then(() => {
-      window.location.reload();
+      setTimeout(() => {
+        window.location.reload();
+      }, 5000);
     });
   };
 
   return (
     <>
+      <Toaster />
       <div className="rounded xl shadow-lg bg-white w-5/6">
         <div className="flex justify-start items-center p-2 gap-3">
           <img className="rounded-full h-14 w-14" src={createdBy.image}></img>
